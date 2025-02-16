9c527a41aa829d4d1f90ac28502eefa3a2dc8f2a
krzysztofpaliga <paligakrzychu@hotmail.com>
2025-02-16 16:26:22 +0100

    from 6.1 to 7

---
+++ O6-conversion/O6-1-from-and-into/src/main.rs
@@ -0,0 +1,31 @@
+use std::convert::From;
+use std::convert::Into;
+
+#[derive(Debug)]
+struct Number {
+    value: i32,
+}
+
+impl From<i32> for Number {
+    fn from(item: i32) -> Self {
+        Number { value: item }
+    }
+}
+
+// impl Into<Number> for i32 {
+//     fn into(self) -> Number {
+//         Number { value: self }
+//     }
+// }
+
+fn main() {
+    let my_str = "hello";
+    let my_string = String::from(my_str);
+
+    let num = Number::from(30);
+    println!("My number is {:?}", num);
+
+    let int = 5;
+    let num: Number = int.into();
+    println!("My number is {:?}", num);
+}
