f749247cc47d1665168094820d93b492c51cf07c
krzysztofpaliga <paligakrzychu@hotmail.com>
2025-02-16 11:45:16 +0100

    from 1 to 3.1

---
+++ O2-primitives/O2-2-2-tuples/src/main.rs
@@ -0,0 +1,57 @@
+use std::fmt;
+
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (int_param, bool_param) = pair;
+    (bool_param, int_param)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+impl fmt::Display for Matrix {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "( {} {} )\n( {} {} )", self.0, self.1, self.2, self.3)
+    }
+}
+
+fn transpose(matrix: Matrix) -> Matrix {
+    let Matrix(a, b, c, d) = matrix;
+    Matrix(a, b, d, c)
+}
+
+fn main() {
+    println!("{:?}", reverse((600, true)));
+
+    let long_tuple = (1u8, 2u16, 3u32, 4u64,
+                      -1i8, -2i16, -3i32, -4i64,
+                      0.1f32, 0.2f64,
+                      'a', true);
+    println!("Long tuple first value: {}", long_tuple.0);
+    println!("Long tuple second value: {}", long_tuple.1);
+   
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    // But long Tuples (more than 12 elements) cannot be printed.
+    //let too_long_tuple = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13);
+    //println!("Too long tuple: {:?}", too_long_tuple);
+    // TODO ^ Uncomment the above 2 lines to see the compiler error
+
+    let pair = (1, true);
+    println!("Pair is {:?}", pair);
+
+    println!("The reversed pais is {:?}", reverse(pair));
+
+    println!("One element tuple: {:?}", (5u32,));
+    println!("Just an integer: {:?}", (5u32));
+
+    let tuple = (1, "hello", 4.5, true);
+
+    let (a, b, c, d) = tuple;
+    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+    println!("{}", matrix);
+    println!("Transpose:\n{}", transpose(matrix));
+}
---
+++ O3-custom-types/O3-1-structures/src/main.rs
@@ -0,0 +1,78 @@
+#![allow(dead_code)]
+
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
+}
+
+struct Unit;
+
+struct Pair(i32, f32);
+
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+struct Rectangle {
+    top_left: Point,
+    bottom_right: Point,
+}
+
+impl Rectangle {
+    fn rect_area(&self) -> f32 {
+        let Rectangle {
+            top_left: Point{ x: x1, y: y1},
+            bottom_right: Point{ x: x2, y: y2},
+        } = *self;
+        let width = (x2 - x1).abs();
+        let height = (y1 - y2).abs();
+        width * height
+    }
+
+    fn square(&self, Point {x: px, y: py}: Point, side: f32) -> Rectangle {
+        Rectangle {
+            top_left: Point { x: px, y: py },
+            bottom_right: Point { x: px - side, y: py + side },
+        }
+    }
+}
+
+fn main() {
+    let name = String::from("Peter");
+    let age = 27;
+    let peter = Person { name, age };
+
+    println!("{:?}", peter);
+
+    let point: Point = Point { x: 5.2, y: 0.4 };
+    let another_point: Point = Point { x: 10.3, y: 0.2 };
+
+    println!("point coordinates: ({}, {})", point.x, point.y);
+
+    let bottom_right = Point { x: 10.3, ..another_point };
+
+    println!("second point: ({}, {})", bottom_right.x,  bottom_right.y);
+
+    let Point { x: left_edge, y: top_edge } = point;
+
+    let rectangle = Rectangle {
+        top_left: Point { x: left_edge, y: top_edge },
+        bottom_right: bottom_right,
+    };
+
+    let area = rectangle.rect_area();
+
+    println!("Area: {}", area);
+
+    let _unit = Unit;
+
+    let pair = Pair(1, 0.1);
+
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
+
+    let Pair(integer, decimal) = pair;
+
+    println!("pair contains {:?} and {:?}", integer, decimal);
+}
